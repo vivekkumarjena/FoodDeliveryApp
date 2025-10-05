4efae40aa0e3ee3b17d3600d5904f77ab5e593b3
vivekkumarjena <147078299+vivekkumarjena@users.noreply.github.com>
2025-10-05 13:19:30 +0530

    Order Update feature integrated successfully

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -6,6 +6,8 @@ import Home from './pages/Home/Home'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopUp from './components/LoginPopUp/LoginPopUp'
+import Verify from './pages/Verify/Verify'
+import MyOrders from './pages/MyOrders/MyOrders'
 
 const App = () => {
 
@@ -20,6 +22,8 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/order' element={<PlaceOrder />} />
+        <Route path='/verify' element={<Verify />} />
+        <Route path='/myorders' element={<MyOrders />} />
       </Routes>
     </div>
     <Footer />
--- FrontEnd/src/components/Navbar/Navbar.jsx
+++ FrontEnd/src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({ setShowLogin }) => {
 
   const { getTotalCartAmount, token, setToken } = useContext(StoreContext)
 
-  const navigate = useNavigate
+  const navigate = useNavigate()
 
   const logout = ()=>{
     localStorage.removeItem("token")
@@ -36,7 +36,7 @@ const Navbar = ({ setShowLogin }) => {
         {!token ? <button onClick={() => setShowLogin(true)}>Sign In</button> : <div className='navbar-profile'>
           <img src={assets.profile_icon} alt="" />
           <ul className='nav-profile-dropdown'>
-            <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+            <li onClick={()=>navigate('/myorders')}><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
             <hr />
             <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>LogOut</p></li>
           </ul>
